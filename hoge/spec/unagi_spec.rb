64b627564094a7ae06b2a95c3f50ce1685bff529
Ryosuke Sato <ryosuke.sato@gi-no.jp>
2014-07-28 23:47:27 +0900

    テストをパスするように修正

--- hoge/spec/unagi_spec.rb
+++ hoge/spec/unagi_spec.rb
@@ -9,13 +9,13 @@ describe Unagi do
 
   describe '#seating' do
     it '「長テーブルの座席番号」と「お客さんの人数」を指定すると「座席にお客さんが配置される」' do
-      unagi = Unagi.new
+      unagi = Unagi.new(5)
       unagi.seating(1, 3)
       expect(unagi.seats).to eq [1, 1, 1, 0, 0]
     end
 
     it '長テーブルの座席は最初と最後の席がループしている' do
-      unagi = Unagi.new
+      unagi = Unagi.new(5)
       unagi.seating(5, 3)
       expect(unagi.seats).to eq [1, 1, 0, 0, 1]
     end
--- hoge/unagi.rb
+++ hoge/unagi.rb
@@ -6,8 +6,6 @@ class Unagi
   end
 
   def seating(seat_number, people_count)
-    @seats = [0, 0, 0, 0, 0]
-
     s = seat_number - 1
     people_count.times do
       @seats[s] = 1
