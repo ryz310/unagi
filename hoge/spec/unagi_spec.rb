719131ba37dd4447fa1cf1ae953c42f3897191ad
Ryosuke Sato <ryosuke.sato@gi-no.jp>
2014-07-28 23:41:52 +0900

    コンストラクタで座席を設定する仕様

--- hoge/spec/unagi_spec.rb
+++ hoge/spec/unagi_spec.rb
@@ -1,6 +1,12 @@
 require './unagi.rb'
 
 describe Unagi do
+  describe '#initialize' do
+    it '指定した座席数のゼロ配列が確保される' do
+      expect(Unagi.new(3).seats).to eq [0, 0, 0]
+    end
+  end
+
   describe '#seating' do
     it '「長テーブルの座席番号」と「お客さんの人数」を指定すると「座席にお客さんが配置される」' do
       unagi = Unagi.new
