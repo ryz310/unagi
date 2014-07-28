6676c5ef583f7c803c0f0a8d09ab016af3a96cf6
Ryosuke Sato <ryosuke.sato@gi-no.jp>
2014-07-28 23:18:14 +0900

    長テーブルの仕様を実装

--- hoge/unagi.rb
+++ hoge/unagi.rb
@@ -1,4 +1,13 @@
 class Unagi
+  attr_reader :seats
+
   def seating(seat_number, people_count)
+    @seats = [0, 0, 0, 0, 0]
+
+    s = seat_number - 1
+    people_count.times do
+      @seats[s] = 1
+      s += 1
+    end
   end
 end
