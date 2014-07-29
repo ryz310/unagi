c37206fd0b17e51eeec301b95d5438de78bd196a
Ryosuke Sato <ryosuke.sato@gi-no.jp>
2014-07-29 13:39:01 +0900

    main処理を記述

--- hoge/unagi.rb
+++ hoge/unagi.rb
@@ -25,3 +25,13 @@ class Unagi
     @seats.select{ |s| s == 1 }.count
   end
 end
+
+if __FILE__ == $0
+  seat_count, group_count = gets.split.map(&:to_i)
+  unagi = Unagi.new(seat_count)
+  group_count.times do
+    people_count, seat_number = gets.split.map(&:to_i)
+    unagi.seating(seat_number, people_count)
+  end
+  puts unagi.count
+end
